0a27efcbc961f80990dc06caa99f53ca35f01b30
Charlie Powell <cdp1337@bitsnbytes.dev>
2025-12-08 10:59:27 -0500

    Initial commit

---
+++ src/installer.sh
@@ -0,0 +1,263 @@
+#!/bin/bash
+#
+# Install Game Server
+#
+# Please ensure to run this script as root (or at least with sudo)
+#
+# @LICENSE AGPLv3
+# @AUTHOR  Charlie Powell <cdp1337@bitsnbytes.dev>
+# @CATEGORY Game Server
+# @TRMM-TIMEOUT 600
+# @WARLOCK-TITLE Game Name
+# @WARLOCK-IMAGE media/some-game-image.webp
+# @WARLOCK-ICON media/some-game-icon.webp
+# @WARLOCK-THUMBNAIL media/some-game-thumbnail.webp
+#
+# Supports:
+#   Debian 12, 13
+#   Ubuntu 24.04
+#
+# Requirements:
+#   None
+#
+# TRMM Custom Fields:
+#   None
+#
+# Syntax:
+#   MODE_UNINSTALL=--uninstall - Perform an uninstallation
+#   OVERRIDE_DIR=--dir=<src> - Use a custom installation directory instead of the default (optional)
+#   SKIP_FIREWALL=--skip-firewall - Do not install or configure a system firewall
+#   NONINTERACTIVE=--non-interactive - Run the installer in non-interactive mode (useful for scripted installs)
+#
+# Changelog:
+#   20251103 - New installer
+
+############################################
+## Parameter Configuration
+############################################
+
+# Name of the game (used to create the directory)
+INSTALLER_VERSION="v20251127~DEV"
+GAME="GameName"
+GAME_DESC="Game Dedicated Server"
+REPO="your-github/your-repo"
+WARLOCK_GUID="replace-with-guid-once-compiled"
+STEAM_ID="123456789"
+GAME_USER="steam"
+GAME_DIR="/home/${GAME_USER}/${GAME}"
+GAME_SERVICE="your-game-server"
+
+# compile:usage
+# compile:argparse
+# scriptlet:_common/require_root.sh
+# scriptlet:_common/get_firewall.sh
+# scriptlet:_common/package_install.sh
+# scriptlet:_common/download.sh
+# scriptlet:bz_eval_tui/prompt_text.sh
+# scriptlet:bz_eval_tui/prompt_yn.sh
+# scriptlet:bz_eval_tui/print_header.sh
+# scriptlet:ufw/install.sh
+# scriptlet:warlock/install_warlock_manager.sh
+
+print_header "$GAME_DESC *unofficial* Installer ${INSTALLER_VERSION}"
+
+############################################
+## Installer Actions
+############################################
+
+##
+# Install the game server
+#
+# Expects the following variables:
+#   GAME_USER    - User account to install the game under
+#   GAME_DIR     - Directory to install the game into
+#   STEAM_ID     - Steam App ID of the game
+#   GAME_DESC    - Description of the game (for logging purposes)
+#   GAME_SERVICE - Service name to install with Systemd
+#   SAVE_DIR     - Directory to store game save files
+#
+function install_application() {
+	print_header "Performing install_application"
+
+	# Create the game user account
+	# This will create the account with no password, so if you need to log in with this user,
+	# run `sudo passwd $GAME_USER` to set a password.
+	if [ -z "$(getent passwd $GAME_USER)" ]; then
+		useradd -m -U $GAME_USER
+	fi
+
+	# Preliminary requirements
+	package_install curl sudo python3-venv
+
+	if [ "$FIREWALL" == "1" ]; then
+		if [ "$(get_enabled_firewall)" == "none" ]; then
+			# No firewall installed, go ahead and install UFW
+			install_ufw
+		fi
+	fi
+
+	[ -e "$GAME_DIR/AppFiles" ] || sudo -u $GAME_USER mkdir -p "$GAME_DIR/AppFiles"
+
+
+	# To download a game with steamcmd, include the following header
+	#  # scriptlet:steam/install-steamcmd.sh
+	# and use 
+	#  install_steamcmd
+	
+	# Install the management script
+	install_warlock_manager "$REPO" "$INSTALLER_VERSION"
+
+	# If other PIP packages are required for your management interface,
+	# add them here as necessary, for example for RCON support:
+	#  sudo -u $GAME_USER $GAME_DIR/.venv/bin/pip install rcon
+	
+	# Use the management script to install the game server
+	if ! $GAME_DIR/manage.py --update; then
+		echo "Could not install $GAME_DESC, exiting" >&2
+		exit 1
+	fi
+	
+	# If you need to configure the firewall for this game service here,
+	# ensure you include the following header
+	# Ideally the management script should handle this if possible to provide the operator with an easy way to change the port.
+	#  # scriptlet:_common/firewall_allow.sh
+	# and then run
+	# firewall_allow --port ${PORT} --udp --comment "${GAME_DESC} Game Port"
+
+	# Install system service file to be loaded by systemd
+    cat > /etc/systemd/system/${GAME_SERVICE}.service <<EOF
+# script:systemd-template.service
+EOF
+    systemctl daemon-reload
+
+	if [ -n "$WARLOCK_GUID" ]; then
+		# Register Warlock
+		[ -d "/var/lib/warlock" ] || mkdir -p "/var/lib/warlock"
+		echo -n "$GAME_DIR" > "/var/lib/warlock/${WARLOCK_GUID}.app"
+	fi
+}
+
+function postinstall() {
+	print_header "Performing postinstall"
+
+	# First run setup
+	$GAME_DIR/manage.py --first-run
+}
+
+##
+# Uninstall the game server
+#
+# Expects the following variables:
+#   GAME_DIR     - Directory where the game is installed
+#   GAME_SERVICE - Service name used with Systemd
+#   SAVE_DIR     - Directory where game save files are stored
+#
+function uninstall_application() {
+	print_header "Performing uninstall_application"
+
+	systemctl disable $GAME_SERVICE
+	systemctl stop $GAME_SERVICE
+
+	# Service files
+	[ -e "/etc/systemd/system/${GAME_SERVICE}.service" ] && rm "/etc/systemd/system/${GAME_SERVICE}.service"
+
+	# Game files
+	[ -d "$GAME_DIR" ] && rm -rf "$GAME_DIR/AppFiles"
+
+	# Management scripts
+	[ -e "$GAME_DIR/manage.py" ] && rm "$GAME_DIR/manage.py"
+	[ -e "$GAME_DIR/configs.yaml" ] && rm "$GAME_DIR/configs.yaml"
+	[ -d "$GAME_DIR/.venv" ] && rm -rf "$GAME_DIR/.venv"
+
+	if [ -n "$WARLOCK_GUID" ]; then
+		# unregister Warlock
+		[ -e "/var/lib/warlock/${WARLOCK_GUID}.app" ] && rm "/var/lib/warlock/${WARLOCK_GUID}.app"
+	fi
+}
+
+############################################
+## Pre-exec Checks
+############################################
+
+if [ $MODE_UNINSTALL -eq 1 ]; then
+	MODE="uninstall"
+else
+	# Default to install mode
+	MODE="install"
+fi
+
+
+if systemctl -q is-active $GAME_SERVICE; then
+	echo "$GAME_DESC service is currently running, please stop it before running this installer."
+	echo "You can do this with: sudo systemctl stop $GAME_SERVICE"
+	exit 1
+fi
+
+if [ -n "$OVERRIDE_DIR" ]; then
+	# User requested to change the install dir!
+	# This changes the GAME_DIR from the default location to wherever the user requested.
+	if [ -e "/var/lib/warlock/${WARLOCK_GUID}.app" ] ; then
+		# Check for existing installation directory based on Warlock registration
+		GAME_DIR="$(cat "/var/lib/warlock/${WARLOCK_GUID}.app")"
+		if [ "$GAME_DIR" != "$OVERRIDE_DIR" ]; then
+			echo "ERROR: $GAME_DESC already installed in $GAME_DIR, cannot override to $OVERRIDE_DIR" >&2
+			echo "If you want to move the installation, please uninstall first and then re-install to the new location." >&2
+			exit 1
+		fi
+	fi
+
+	GAME_DIR="$OVERRIDE_DIR"
+	echo "Using ${GAME_DIR} as the installation directory based on explicit argument"
+elif [ -e "/var/lib/warlock/${WARLOCK_GUID}.app" ]; then
+	# Check for existing installation directory based on service file
+	GAME_DIR="$(cat "/var/lib/warlock/${WARLOCK_GUID}.app")"
+	echo "Detected installation directory of ${GAME_DIR} based on service registration"
+else
+	echo "Using default installation directory of ${GAME_DIR}"
+fi
+
+if [ -e "/etc/systemd/system/${GAME_SERVICE}.service" ]; then
+	EXISTING=1
+else
+	EXISTING=0
+fi
+
+############################################
+## Installer
+############################################
+
+
+if [ "$MODE" == "install" ]; then
+
+	if [ $SKIP_FIREWALL -eq 1 ]; then
+		FIREWALL=0
+	elif [ $EXISTING -eq 0 ] && prompt_yn -q --default-yes "Install system firewall?"; then
+		FIREWALL=1
+	else
+		FIREWALL=0
+	fi
+
+	install_application
+
+	postinstall
+
+	# Print some instructions and useful tips
+    print_header "$GAME_DESC Installation Complete"
+fi
+
+if [ "$MODE" == "uninstall" ]; then
+	if [ $NONINTERACTIVE -eq 0 ]; then
+		if prompt_yn -q --invert --default-no "This will remove all game binary content"; then
+			exit 1
+		fi
+		if prompt_yn -q --invert --default-no "This will remove all player and map data"; then
+			exit 1
+		fi
+	fi
+
+	if prompt_yn -q --default-yes "Perform a backup before everything is wiped?"; then
+		$GAME_DIR/manage.py --backup
+	fi
+
+	uninstall_application
+fi
---
+++ src/manage.py
@@ -0,0 +1,238 @@
+#!/usr/bin/env python3
+import pwd
+import random
+import string
+from scriptlets._common.firewall_allow import *
+from scriptlets._common.firewall_remove import *
+from scriptlets.bz_eval_tui.prompt_yn import *
+from scriptlets.bz_eval_tui.prompt_text import *
+from scriptlets.bz_eval_tui.table import *
+from scriptlets.bz_eval_tui.print_header import *
+from scriptlets._common.get_wan_ip import *
+# import:org_python/venv_path_include.py
+import yaml
+# Game application source - what type of game is being installed?
+# from scriptlets.warlock.base_app import *
+# from scriptlets.warlock.steam_app import *
+# Game services are usually either an RCON, HTTP, or base type service.
+# Include the necessary type and remove the rest.
+# from scriptlets.warlock.base_service import *
+# from scriptlets.warlock.http_service import *
+# from scriptlets.warlock.rcon_service import *
+from scriptlets.warlock.ini_config import *
+from scriptlets.warlock.properties_config import *
+from scriptlets.warlock.default_run import *
+
+# For games that use Steam, this provides a quick method for checking for updates
+# from scriptlets.steam.steamcmd_check_app_update import *
+
+here = os.path.dirname(os.path.realpath(__file__))
+
+
+class GameApp(SteamApp):
+	"""
+	Game application manager
+	"""
+
+	def __init__(self):
+		super().__init__()
+
+		self.name = 'GameName'
+		self.desc = 'Longer identifier for the game server'
+		self.steam_id = '123456789'
+		self.services = ('list-of-services',)
+
+		self.configs = {
+			'manager': INIConfig('manager', os.path.join(here, '.settings.ini'))
+		}
+		self.load()
+
+	def get_save_files(self) -> Union[list, None]:
+		"""
+		Get a list of save files / directories for the game server
+
+		:return:
+		"""
+		files = ['banned-ips.json', 'banned-players.json', 'ops.json', 'whitelist.json']
+		for service in self.get_services():
+			files.append(service.get_name())
+		return files
+
+	def get_save_directory(self) -> Union[str, None]:
+		"""
+		Get the save directory for the game server
+
+		:return:
+		"""
+		return os.path.join(here, 'AppFiles')
+
+
+class GameService(RCONService):
+	"""
+	Service definition and handler
+	"""
+	def __init__(self, service: str, game: GameApp):
+		"""
+		Initialize and load the service definition
+		:param file:
+		"""
+		super().__init__(service, game)
+		self.service = service
+		self.game = game
+		self.configs = {
+			'server': PropertiesConfig('server', os.path.join(here, 'AppFiles/server.properties'))
+		}
+		self.load()
+
+	def option_value_updated(self, option: str, previous_value, new_value):
+		"""
+		Handle any special actions needed when an option value is updated
+		:param option:
+		:param previous_value:
+		:param new_value:
+		:return:
+		"""
+
+		# Special option actions
+		if option == 'Server Port':
+			# Update firewall for game port change
+			if previous_value:
+				firewall_remove(int(previous_value), 'tcp')
+			firewall_allow(int(new_value), 'tcp', 'Allow %s game port' % self.game.desc)
+		elif option == 'Query Port':
+			# Update firewall for game port change
+			if previous_value:
+				firewall_remove(int(previous_value), 'udp')
+			firewall_allow(int(new_value), 'udp', 'Allow %s query port' % self.game.desc)
+
+	def is_api_enabled(self) -> bool:
+		"""
+		Check if API is enabled for this service
+		:return:
+		"""
+		return (
+			self.get_option_value('Enable RCON') and
+			self.get_option_value('RCON Port') != '' and
+			self.get_option_value('RCON Password') != ''
+		)
+
+	def get_api_port(self) -> int:
+		"""
+		Get the API port from the service configuration
+		:return:
+		"""
+		return self.get_option_value('RCON Port')
+
+	def get_api_password(self) -> str:
+		"""
+		Get the API password from the service configuration
+		:return:
+		"""
+		return self.get_option_value('RCON Password')
+
+	def get_player_count(self) -> Union[int, None]:
+		"""
+		Get the current player count on the server, or None if the API is unavailable
+		:return:
+		"""
+		try:
+			ret = self._api_cmd('/list')
+			# ret should contain 'There are N of a max...' where N is the player count.
+			if ret is None:
+				return None
+			elif 'There are ' in ret:
+				return int(ret[10:ret.index(' of a max')].strip())
+			else:
+				return None
+		except:
+			return None
+
+	def get_player_max(self) -> int:
+		"""
+		Get the maximum player count allowed on the server
+		:return:
+		"""
+		return self.get_option_value('Max Players')
+
+	def get_name(self) -> str:
+		"""
+		Get the name of this game server instance
+		:return:
+		"""
+		return self.get_option_value('Level Name')
+
+	def get_port(self) -> Union[int, None]:
+		"""
+		Get the primary port of the service, or None if not applicable
+		:return:
+		"""
+		return self.get_option_value('Server Port')
+
+	def get_game_pid(self) -> int:
+		"""
+		Get the primary game process PID of the actual game server, or 0 if not running
+		:return:
+		"""
+
+		# For services that do not have a helper wrapper, it's the same as the process PID
+		return self.get_pid()
+
+		# For services that use a wrapper script, the actual game process will be different and needs looked up.
+		'''
+		# There's no quick way to get the game process PID from systemd,
+		# so use ps to find the process based on the map name
+		processes = subprocess.run([
+			'ps', 'axh', '-o', 'pid,cmd'
+		], stdout=subprocess.PIPE).stdout.decode().strip()
+		exe = os.path.join(here, 'AppFiles/Vein/Binaries/Linux/VeinServer-Linux-')
+		for line in processes.split('\n'):
+			pid, cmd = line.strip().split(' ', 1)
+			if cmd.startswith(exe):
+				return int(line.strip().split(' ')[0])
+		return 0
+		'''
+
+	def send_message(self, message: str):
+		"""
+		Send a message to all players via the game API
+		:param message:
+		:return:
+		"""
+		self._api_cmd('/say %s' % message)
+
+	def save_world(self):
+		"""
+		Force the game server to save the world via the game API
+		:return:
+		"""
+		self._api_cmd('save-all flush')
+
+
+def menu_first_run(game: GameApp):
+	"""
+	Perform first-run configuration for setting up the game server initially
+
+	:param game:
+	:return:
+	"""
+	print_header('First Run Configuration')
+
+	if os.geteuid() != 0:
+		print('ERROR: Please run this script with sudo to perform first-run configuration.')
+		sys.exit(1)
+
+	svc = game.get_services()[0]
+
+	svc.option_ensure_set('Level Name')
+	svc.option_ensure_set('Server Port')
+	svc.option_ensure_set('RCON Port')
+	if not svc.option_has_value('RCON Password'):
+		# Generate a random password for RCON
+		random_password = ''.join(random.choices(string.ascii_letters + string.digits, k=32))
+		svc.set_option('RCON Password', random_password)
+	if not svc.option_has_value('Enable RCON'):
+		svc.set_option('Enable RCON', True)
+
+if __name__ == '__main__':
+	game = GameApp()
+	run_manager(game)
